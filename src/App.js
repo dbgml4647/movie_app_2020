8e20f72a4f48932260dc98c28290af702493b104
Yuhee <dbgml4647@naver.com>
2020-04-09 22:50:09 +0900

    https://github.com/dbgml4647/movie_app_2020.git

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
-
-function Food(props){
-
-  return  <h2>I Love {props.name}</h2>
-
-}
 
 //const foodILike = [{}]
- // 객제 배열
+// 객제 배열
+
+function Food({ name,picture,rating}) {
+  return <div>
+    <h2>I Love {name}</h2>
+    <h4>{ rating}/5</h4>
+    <img src = {picture} alt="main dish"/>
+
+  </div>
+}
+
+
+Food.propTypes = {
+  name : PropTypes.string.isRequired,
+  picture : PropTypes.string.isRequired,
+  rating : PropTypes.number.isRequired
+
+
+};
+
+
+const foodILike = [
+  {
+    id :1,
+    name: "떡볶이",
+    image: "http://www.jawsfood.com/uploads/product/20180605409363.png",
+    rating:5
+
+
+  },
+  {
+    id : 2, 
+    name: "순대",
+    image: "https://www.jbplaza.com/data/shop/item/1453801573_l1",
+    rating :4.8
+
+  },
+  {
+    id : 3,
+    name: "감자탕",
+    image: "http://www.businesskorea.co.kr/news/photo/201905/31575_43222_232.jpg",
+    rating : 4.7
+  }
+];
+
 
 function App() {
   return (
     <div className="App">
-      Hello!
-      <Food name="kimchi"/>
-      <Food name="떡볶이"/>
-      <Food name="순대"/>
-      <Food name="감자탕"/>
- 
+      
+      {foodILike.map(dish =>  <Food key = {dish.id} name = {dish.name} picture = {dish.image} rating = {dish.rating}/>)}
+
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //import './index.css';
 import App from './App';
+//import Potato from './Potato';
 //import * as serviceWorker from './serviceWorker';
-//import Potato from "./Potato";
 
 ReactDOM.render(<App />, document.getElementById('root')
 );
