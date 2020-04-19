04c54602041552d137b1ad1a8759c345a5f95774
Yuhee <dbgml4647@naver.com>
2020-04-20 02:43:12 +0900

    https://github.com/dbgml4647/movie_app_2020.git

--- src/App.js
+++ src/App.js
@@ -1,63 +1,36 @@
 import React from 'react';
-import PropTypes from "prop-types";
 
 
 //const foodILike = [{}]
 // 객제 배열
 
-function Food({ name,picture,rating}) {
-  return <div>
-    <h2>I Love {name}</h2>
-    <h4>{ rating}/5</h4>
-    <img src = {picture} alt="main dish"/>
+class App extends React.Component{
 
-  </div>
-}
+  state={
+    count : 0
 
 
-Food.propTypes = {
-  name : PropTypes.string.isRequired,
-  picture : PropTypes.string.isRequired,
-  rating : PropTypes.number.isRequired
+  };
+
+  add = () => {
+    this.setState(current => ({ count : current.count + 1}));
+  };
+  minus = () => {
+    this.setState( current => ({ count : current.count - 1}));
+  }; 
+// react에서 외부의 상태에 의존 하지 않는 가장 좋은 방법
 
 
-};
-
-
-const foodILike = [
-  {
-    id :1,
-    name: "떡볶이",
-    image: "http://www.jawsfood.com/uploads/product/20180605409363.png",
-    rating:5
-
-
-  },
-  {
-    id : 2, 
-    name: "순대",
-    image: "https://www.jbplaza.com/data/shop/item/1453801573_l1",
-    rating :4.8
-
-  },
-  {
-    id : 3,
-    name: "감자탕",
-    image: "http://www.businesskorea.co.kr/news/photo/201905/31575_43222_232.jpg",
-    rating : 4.7
-  }
-];
-
-
-function App() {
+//여기는 return 이 없다.
+render(){
   return (
-    <div className="App">
-      
-      {foodILike.map(dish =>  <Food key = {dish.id} name = {dish.name} picture = {dish.image} rating = {dish.rating}/>)}
-
-
-    </div>
-  );
+  <div>  
+    The count is {this.state.count}
+    <button onClick={this.add}>Add</button>
+    <button onClick = {this.minus}>Minus</button>
+    
+    </div>)
+}
 }
 
 export default App;
