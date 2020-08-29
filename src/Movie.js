843ae1a60aaa872cf94efb01d5153b839fec0c54
Yuhee <dbgml4647@naver.com>
2020-08-29 18:44:09 +0900

    장르 추가하기

--- src/Movie.js
+++ src/Movie.js
@@ -1,13 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Movie({ id, year, title, summary, poster }) {
+function Movie({ id, year, title, summary, poster, genres }) {
   return (
     <div class = "movie">
       <img src={poster} alt={title} title={title}></img>
     <div class="movie_data">     
     <h3 class = "movie_title">{title}</h3>
     <h5 class = "movie_year">{title}</h5>
+    <ul class="genres"> 
+    {genres.map((genre,index) => (
+    <li key={index} className = "genres_genre">{genre}</li>))}</ul>
     <p class="movie_summary">{summary}</p>
     </div>
     </div>
@@ -20,7 +23,8 @@ Movie.propTypes = {
   year: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
-  poster: PropTypes.string.isRequired
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 //git 사용하기
